eaaa699eb17f515a5ab603966632d0a44c0c4a2b
hihilkh <hihilkh@gmail.com>
2022-12-31 16:27:28 +0900

    inline関数を使う

--- src/Utils/Math.h
+++ src/Utils/Math.h
@@ -1,10 +1,14 @@
 ﻿#pragma once
 
+#include <cmath>
+
 namespace Math
 {
 	/// <summary>
 	/// c++の負数の割り算は0への丸め(rounding toward zero)で、 -1 / 2 = 0　になる<para />
 	/// -1 / 2 = -1 をしたい場合、この関数を使う
 	/// </summary>
-	int FloorDivide(int dividend, int divisor);
+	inline int FloorDivide(int dividend, int divisor) {
+		return (int)floor(float(dividend) / divisor);
+	}
 }
