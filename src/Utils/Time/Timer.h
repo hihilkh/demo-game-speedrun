c218555113d1da42f023606372ca4e4d38087427
hihilkh <hihilkh@gmail.com>
2023-01-29 02:03:30 +0900

    タイマーを追加

---
+++ src/Utils/Time/Timer.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include <chrono>
+
+namespace Time
+{
+	class Timer
+	{
+	public:
+		Timer();
+		~Timer();
+
+	private:
+		bool isStarted;
+		bool isRunning;
+		std::chrono::time_point<std::chrono::steady_clock> startTime;
+		std::chrono::milliseconds durationForPause;
+
+	private:
+		std::chrono::milliseconds GetCurrentDuration() const;
+
+	public:
+		void Start();
+		void Pause();
+		void Unpause();
+		void Reset();
+		int GetCurrentCountMillisecond() const;
+	};
+}
