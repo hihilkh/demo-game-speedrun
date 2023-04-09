f91e0195a77cc9fb4d262ca25af0d02a8949abf3
hihilkh <hihilkh@gmail.com>
2023-04-09 23:31:55 +0900

    Log.hで DEBUG_IS_ENABLED マクロを使う

--- src/Utils/Log.h
+++ src/Utils/Log.h
@@ -1,38 +1,32 @@
 ﻿#pragma once
 
-#ifdef _DEBUG
+// TODO : このファイルをDebugフォルダーへ移す
+// TODO : コードスタンダードに合わせるように
+#include "Debug/DebugConfig.h"
+
+#if DEBUG_IS_ENABLED(DEBUG_CONSOLE_LOGGING)
 
 #include <sstream>
 #include <windows.h>
 
-#else
-
-#endif // _DEBUG
-
-namespace Log {
-
-#ifdef _DEBUG
-
-	#define PrintSameLine(message) {\
-		std::stringstream ss;\
-		ss << message;\
-		OutputDebugString(ss.str().c_str());\
-	}
-
-	#define Print(message)\
-		PrintSameLine(message);\
-		OutputDebugString("\n")
-
-	#define PrintWarningSameLine(message)	PrintSameLine("!! : " << message)
-	#define PrintWarning(message)			Print("!! : " << message)
-
-#else
-
-	#define PrintSameLine(message)
-	#define Print(message)
-	#define PrintWarningSameLine(message)
-	#define PrintWarning(message)
-
-#endif // _DEBUG
-
+#define PrintSameLine(message) {\
+	std::stringstream ss;\
+	ss << message;\
+	OutputDebugString(ss.str().c_str());\
 }
+
+#define Print(message)\
+	PrintSameLine(message);\
+	OutputDebugString("\n")
+
+#define PrintWarningSameLine(message)	PrintSameLine("!! : " << message)
+#define PrintWarning(message)			Print("!! : " << message)
+
+#else
+
+#define PrintSameLine(message)
+#define Print(message)
+#define PrintWarningSameLine(message)
+#define PrintWarning(message)
+
+#endif
